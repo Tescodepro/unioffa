dc3074c2c6b19f7e419813ccfd045cf4bdf3a6da
Tescodepro <tescodepro@gmail.com>
2026-03-29 07:11:16 +0100

    update letter head latesr

--- resources/views/applications/admission-letter.blade.php
+++ resources/views/applications/admission-letter.blade.php
@@ -27,7 +27,7 @@
         .letterhead {
             width: 100%;
             position: fixed;
-            top: 40px;
+            top: 100px;
             left: 0;
             z-index: 10;
         }
@@ -53,7 +53,7 @@
 
         /* Main Content Container */
         .content {
-            padding: 150px 25mm 100px 25mm;
+            padding: 50px 25mm 100px 25mm;
             /* Top space for letterhead, sides for margins */
             font-size: 11.5pt;
             line-height: 1.6;
